5446542c0296568399311ad43f7ef7c0c0daee96
Kiran Kumar <kiran.kumar@intelliswift.com>
2025-08-12 10:56:50 +0530

    test

---
+++ variable.tf
@@ -0,0 +1,40 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region to deploy resources"
+  type        = string
+}
+
+variable "region_2" {
+  description = "The GCP region to deploy resources"
+  type        = string
+}
+
+variable "region_3" {
+  description = "The GCP region to deploy resources"
+  type        = string
+}
+
+variable "zone" {
+  description = "The GCP zone to deploy the VM"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "The machine type for the VM"
+  type        = string
+  default     = "demo.wsd"
+}
+
+variable "vm_name" {
+  description = "The name of the VM"
+  type        = string
+}
+
+variable "service_account_email" {
+  description = "The service account email"
+  type        = string
+}
