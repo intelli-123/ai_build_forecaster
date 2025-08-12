21ede7aa9cb35e576e732a8dde965940b5987c27
Kiran Kumar <kiran.kumar@intelliswift.com>
2025-08-12 19:56:52 +0530

    new tesy

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ resource "google_compute_instance" "vm" {
   zone         = var.zone
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-12
-      size  = 40
+      image = "debian-cloud/debian-12"
+      size  = 4o
     }
   }
   network_interface {
