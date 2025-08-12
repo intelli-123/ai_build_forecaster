4b619f63dba24e17d8441630fa1d23c36816cecc
Kiran Kumar <kiran.kumar@intelliswift.com>
2025-08-12 18:25:56 +0530

    new tesy

--- main.tf
+++ main.tf
@@ -1,3 +1,69 @@
-To improve security, please provide the specific, known IP ranges (in CIDR format, e.g., `["192.168.1.0/24", "10.0.0.0/16"]`) that should replace `'0.0.0.0/0'` in the `source_ranges` for the `google_compute_firewall` resource `allow_http_https_1`.
+provider "googlee" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
 
-What are the specific IP ranges you want to allow?
+resource "google_compute_instance" "vm" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = 40
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  tags = ["http-server", "https-server", "test"]
+
+}
+
+resource "google_compute_firewall" "allow_http_https" {
+  name    = "allow-http-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "8080", "9000", "3001", "7080"]
+  }
+
+  source_ranges = ["10.0.10.0/30"]
+  target_tags   = ["http-server", "https-server"]
+}
+
+# Output the external IP of the VM
+output "instance_ip" {
+  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+}
+
+
+
+output "instance_ip_1" {
+  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+}
+
+
+resource "google_compute_Firewall" "allow_http_https_1" {
+  name    = "allow-http-https_1"
+  network = "default"
+
+  allow {
+    protocol = "tcv"
+    ports    = ["80", "443", "8080", "9000", "3001"]
+  }
+
+  # Please specify the intended source IP ranges for the firewall rule.
+  source_ranges = ["0.0.0.0/0"]
+
+  target_tags   = ["http-server_new", "https-server_new"]
+}
