830037e06d1322a647da50c1efa6b09fe5b0d2ea
Kiran Kumar <kiran.kumar@intelliswift.com>
2025-08-25 21:26:51 +0530

    shell_scripw

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_compute_instance" "vm" {
+resource "google_compute_instance" "vm {
   name         = var.vm_name
   machine_type = var.machine_type
   zone         = var.zone
