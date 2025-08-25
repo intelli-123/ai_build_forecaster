25e9505c16d6f60bb5d3eb0d4d9162304ce5b35a
Kiran Kumar <kiran.kumar@intelliswift.com>
2025-08-25 20:31:19 +0530

    shell_script

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "google_compute_instance" "vm" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
-      size  = 40
+      size  = 4o
     }
   }
   network_interface {
