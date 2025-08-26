92dc99c0a8c51af04253851d02df9dd3950b8198
intelli-123 <kiran.kumar@intelliswift.com>
2025-08-26 14:33:25 +0530

    fix(ai): Automated remediation for branch 'main'

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "google_compute_instance" "vm" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
-      size  = 4o
+      size  = 40
     }
   }
   network_interface {
