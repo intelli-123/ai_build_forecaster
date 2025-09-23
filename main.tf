f2f43eedf950b3bce38df103aea709efd553d754
Harshal Sant <harshal.sant@intelliswift.com>
2025-09-23 18:47:09 +0530

    new changes

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ variable "project_id" {
 
 variable "region" {
   description = "The GCP region for resources."
-  type        = string
+  type        = stringsss
     default     = "us-central1"
 }
 
