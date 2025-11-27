6b6a913f369c6d51112e6e09e0f938aa5700a1a4
intelli-123 <kiran.kumar@intelliswift.com>
2025-11-27 12:25:28 +0530

    40

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "google_compute_instance" "vm" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
-      size   = 40
+      size   = 4o
     }
   }
   network_interface {
